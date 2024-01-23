c1960e852343fb7d2de200668a145007d998607f
sjr <sjroberts@gmail.com>
2024-01-23 15:08:29 +0000

    Fixing bug

--- src/main/java/com/scrumcenter/csd/refactoring/polymorphism/Player.java
+++ src/main/java/com/scrumcenter/csd/refactoring/polymorphism/Player.java
@@ -11,7 +11,7 @@ public class Player {
         return switch (playerType) {
             case "Warrior" -> "Warrior moves forward";
             case "Mage" -> "Mage teleports";
-            case "Rogue" -> "Rogue freaks";
+            case "Rogue" -> "Rogue sneaks";
             default -> throw new IllegalArgumentException("Invalid player type");
         };
     }
